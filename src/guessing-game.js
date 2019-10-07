44448cb0f959047c44232256566ba9be436fe871
LazouskiKiryl <kiryllazouski@gmail.com>
2019-10-07 23:57:26 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.result = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.result = Math.ceil( (this.max + this.min) / 2 );
     }
 
     lower() {
-
+        this.max = this.result;
     }
-
+    
     greater() {
-
+        this.min = this.result;
     }
 }
 
